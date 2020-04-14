905cf98e73506a5d319ba4f7916b18ce79eb1d2f
Unknown <Call5h5ong@gmail.com>
2020-04-14 12:12:15 +0900

    React Router를 사용하기 위해 Header 위치 변경

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './Home/Home';
+import Header from '../components/Header/Header';
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Route exact path="/" component={Home} />
     </BrowserRouter>
   );
