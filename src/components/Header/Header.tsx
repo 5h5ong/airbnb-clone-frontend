ba0091afb2f232e1c26d0d6eb1da6b23c9b066f8
Unknown <Call5h5ong@gmail.com>
2020-04-20 17:48:19 +0900

    스크롤 시 Header 하단에 그림자 표시
    처음에는 그림자가 없지만 스크롤를 내리는 순간부터 `Header`의 하단에 그림자가 표시됨.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import useScroll from '../../hooks/useScroll';
+import AirbnbLogo from '../../icons/AirbnbLogo';
+import HeaderButton from './HeaderButton';
 
 const HeaderBase = styled.div`
+  display: flex;
+  background-color: #ffffff;
+  color: #ff385c;
+  justify-content: space-between;
+  align-items: center;
   position: fixed;
   width: 100%;
   height: 80px;
+  padding-left: 1rem;
+  padding-right: 1rem;
 `;
-const HeaderContainerFirst = styled(HeaderBase)`
-  background-color: black;
-  /* opacity: 0.1; */
-  color: white;
-`;
+const HeaderContainerFirst = styled(HeaderBase)``;
 const HeaderContainerSecond = styled(HeaderBase)`
-  background-color: height;
-  color: black;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
 `;
 
 const Header: React.FC = () => {
   const { verticalScroll } = useScroll();
   return verticalScroll === 0 ? (
-    <HeaderContainerFirst>{verticalScroll}</HeaderContainerFirst>
+    <HeaderContainerFirst>
+      <AirbnbLogo />
+      <HeaderButton />
+    </HeaderContainerFirst>
   ) : (
-    <HeaderContainerSecond>Header is Changed!</HeaderContainerSecond>
+    <HeaderContainerSecond>
+      <AirbnbLogo />
+      <HeaderButton />
+    </HeaderContainerSecond>
   );
 };
 
