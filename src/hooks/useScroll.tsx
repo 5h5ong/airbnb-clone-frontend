07a02078d8826152301905fd98f4189f4894c900
Unknown <Call5h5ong@gmail.com>
2020-04-28 16:46:54 +0900

    스크롤이 가장 밑에 도달했는지 알려주는 `isEnd` return 값 추가

--- src/hooks/useScroll.tsx
+++ src/hooks/useScroll.tsx
@@ -2,15 +2,32 @@ import { useState, useEffect } from 'react';
 
 interface IUseScrollReturnType {
   verticalScroll: number;
+  /** 스크롤이 끝까지 내려갔는지 */
+  isEnd: boolean;
 }
 
 const useScroll = (): IUseScrollReturnType => {
   const [verticalScroll, setVerticalScroll] = useState(0);
+  const [isEnd, setIsEnd] = useState(false);
+  // const [isTop, setIsTop] = useState(false);
 
   useEffect(() => {
     const handleScroll = (): void => {
-      const { scrollTop } = document.documentElement;
+      const {
+        scrollTop,
+        scrollHeight,
+        clientHeight,
+      } = document.documentElement;
+
+      // scrollTop의 값.
       setVerticalScroll(scrollTop);
+
+      // 스크롤이 끝까지 내려갔는지 판단
+      if (scrollHeight - scrollTop === clientHeight) {
+        setIsEnd(true);
+      } else {
+        setIsEnd(false);
+      }
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -18,7 +35,7 @@ const useScroll = (): IUseScrollReturnType => {
     return (): void => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return { verticalScroll };
+  return { verticalScroll, isEnd };
 };
 
 export default useScroll;
