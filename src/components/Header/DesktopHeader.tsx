d158ec9e37964c40b0f95ec4cf569666b88e9f4c
Unknown <Call5h5ong@gmail.com>
2020-05-08 11:20:25 +0900

    Header의 초기 상태를 투명하게 만들 수 있는 기능 추가
    + 약간의 변수 이름 리팩토링

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -5,25 +5,35 @@ import useScroll from '../../hooks/useScroll';
 import AirbnbLogo from '../../icons/AirbnbLogo';
 import HeaderButton from './HeaderButton';
 
+interface DesktopHeaderProps {
+  /** 초기 상태를 투명하게 만들 것인지 */
+  transparency?: boolean;
+}
+
 const HeaderBase = styled.div`
   display: flex;
   z-index: 100;
-  background-color: #ffffff;
-  color: #ff385c;
   justify-content: space-between;
   align-items: center;
   position: fixed;
   width: 100%;
   height: 80px;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding-left: 80px;
+  padding-right: 80px;
 `;
-const HeaderContainerFirst = styled(HeaderBase)``;
-const HeaderContainerSecond = styled(HeaderBase)`
+const HeaderContainerTransparency = styled(HeaderBase)`
+  background-color: transparent;
+  color: white;
+`;
+const HeaderContainerDefault = styled(HeaderBase)`
+  background-color: #ffffff;
+  color: #ff385c;
+`;
+const HeaderContainerShadow = styled(HeaderContainerDefault)`
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.18);
 `;
 
-const DesktopHeader: React.FC = () => {
+const DesktopHeader: React.FC<DesktopHeaderProps> = ({ transparency }) => {
   const { isTop } = useScroll();
   const history = useHistory();
 
@@ -31,20 +41,37 @@ const DesktopHeader: React.FC = () => {
     history.push('/');
   };
 
+  if (transparency) {
+    return isTop ? (
+      <HeaderContainerTransparency>
+        <div onClick={onClickLogo}>
+          <AirbnbLogo />
+        </div>
+        <HeaderButton />
+      </HeaderContainerTransparency>
+    ) : (
+      <HeaderContainerShadow>
+        <div onClick={onClickLogo}>
+          <AirbnbLogo />
+        </div>
+        <HeaderButton />
+      </HeaderContainerShadow>
+    );
+  }
   return isTop ? (
-    <HeaderContainerFirst>
+    <HeaderContainerDefault>
       <div onClick={onClickLogo}>
         <AirbnbLogo />
       </div>
       <HeaderButton />
-    </HeaderContainerFirst>
+    </HeaderContainerDefault>
   ) : (
-    <HeaderContainerSecond>
+    <HeaderContainerShadow>
       <div onClick={onClickLogo}>
         <AirbnbLogo />
       </div>
       <HeaderButton />
-    </HeaderContainerSecond>
+    </HeaderContainerShadow>
   );
 };
 
