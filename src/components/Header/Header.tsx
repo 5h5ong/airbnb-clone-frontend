0b9887a28c9c34dd0f065ee245e7aa8351440b37
Unknown <Call5h5ong@gmail.com>
2020-05-08 11:21:31 +0900

    특정 페이지에서 헤더의 타입을 바꿀 수 있도록 만듬
    투명한 헤더 그거 있잖아 그거...

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import checkDevice from '../../Functions/checkDevice';
 import DesktopHeader from './DesktopHeader';
 import MobileHeader from './MobileHeader';
@@ -8,9 +9,21 @@ import useDeviceWidth from '../../hooks/useDeviceWidth';
  * Desktop, Mobile에 적절한 Header Component를 반환함
  */
 const Header: React.FC = () => {
+  const { pathname } = useLocation();
   const { width } = useDeviceWidth();
   const { type: deviceType } = checkDevice(width);
 
+  if (pathname === '/experiences') {
+    return (
+      <div>
+        {deviceType === 'desktop' ? (
+          <DesktopHeader transparency />
+        ) : (
+          <MobileHeader />
+        )}
+      </div>
+    );
+  }
   return (
     <div>{deviceType === 'desktop' ? <DesktopHeader /> : <MobileHeader />}</div>
   );
