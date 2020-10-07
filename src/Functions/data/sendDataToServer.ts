689a2eeb39908caee47825c82ac4e96857faac0c
call5h5ong <Call5h5ong@gmail.com>
2020-10-07 21:56:18 +0900

    Return AxiosResponse
    axios에서 받은 데이터를 반환하게 만듬

--- src/Functions/data/sendDataToServer.ts
+++ src/Functions/data/sendDataToServer.ts
@@ -1,15 +1,16 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export const sendDataToServer = async (
   url: string,
   data: {}
-): Promise<void> => {
+): Promise<AxiosResponse> => {
   try {
-    await axios({
+    const responseData = await axios({
       method: 'post',
       url,
       data,
     });
+    return responseData;
   } catch (error) {
     throw new Error('데이터를 보내는 중 에러가 발생하였습니다.');
   }
