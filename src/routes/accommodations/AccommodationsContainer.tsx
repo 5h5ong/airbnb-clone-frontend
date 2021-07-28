0064cd4402f54b63cae1c77678b3447d56104345
call5h5ong <call5h5ong@gmail.com>
2021-07-28 12:34:26 +0900

    feat: 숙소 페이지 생성
    기본 레이아웃만 잡아놓음. 후에 기능구현 예정(맵, 숙소목록 탐색).

---
+++ src/routes/accommodations/AccommodationsContainer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AccommodationsPresenter from './AccommodationsPresenter';
+
+const AccommodationsContainer: React.FC = () => {
+  return <AccommodationsPresenter />;
+};
+
+export default AccommodationsContainer;
---
+++ src/routes/accommodations/AccommodationsPresenter.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PageLayoutWithDivide from '../../components/Layouts/PageLayoutWithDivide';
+
+const AccommodationsPresenter: React.FC = () => {
+  return (
+    <PageLayoutWithDivide
+      image="accommodationsPageImage.jpeg"
+      subTitle="편안한 당신의 여행을 위한 에어비엔비 숙소"
+      title="숙소"
+      description="모두가 함께 즐기는 세계 각지의 멋진 숙소"
+    >
+      <div>HELLO TEST</div>
+    </PageLayoutWithDivide>
+  );
+};
+
+export default AccommodationsPresenter;
