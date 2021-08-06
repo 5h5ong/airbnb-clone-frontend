bee1bc21117bf1703f7538593f4bd2264150c1dc
call5h5ong <call5h5ong@gmail.com>
2021-08-06 14:36:58 +0900

    feat: 숙소 리스트 생성 및 표시
    숙소 페이지에서 숙소 데이터를 받아 리스트 형식으로 표시함. description이 포함된 데이터가 없어서 의미없는 텍스트를 적어놓음. 추후 수정 필요.

---
+++ src/components/Cards/AccmmodationsCard.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from 'styled-components';
+import ColumnSpaceWrapper from '../Wrappers/ColumnSpaceWrapper';
+import RowSpaceWrapper from '../Wrappers/RowSpaceWrapper';
+
+interface AccommodationsCardProps {
+  name: string;
+  image: string[];
+  price: string;
+  description: string;
+}
+interface MarginStyledType {
+  gap: number;
+}
+interface ThumbnailStyledType {
+  imageUrl: string;
+}
+
+const AccommodationsCard: React.FC<AccommodationsCardProps> = ({
+  name,
+  price,
+  description,
+  image,
+}) => {
+  const Margin = styled.div<MarginStyledType>`
+    margin-bottom: ${(props) => `${props.gap}px`};
+  `;
+  const DividerBase = styled.div`
+    width: 70px;
+    border-bottom: 1px solid #ebebeb;
+  `;
+  const DividerWithMargin = styled(DividerBase)`
+    margin-top: 20px;
+    margin-bottom: 20px;
+  `;
+  const Divider = styled(DividerBase)`
+    width: 100%;
+    margin-left: 40px;
+  `;
+  const Container = styled.div`
+    width: 100%;
+    padding: 40px;
+  `;
+  const Thumbnail = styled.div<ThumbnailStyledType>`
+    background-color: gray;
+    border-radius: 4px;
+    width: 300px;
+    height: 200px;
+    background: ${(props) => `url('${props.imageUrl}')`};
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+  `;
+  const TextContainer = styled.div`
+    /* 옆의 썸네일과 어울리게 위쪽에 빈공간을 줌 */
+    padding-top: 10px;
+  `;
+  // 숙소 이름
+  const Name = styled.div`
+    font-size: 25pt;
+  `;
+  // 숙소 가격
+  const Price = styled.div`
+    font-size: 20pt;
+    margin-bottom: 20px;
+  `;
+  // 숙소 설명
+  const Description = styled.div`
+    font-size: 12pt;
+  `;
+
+  return (
+    <ColumnSpaceWrapper gap={10}>
+      <Container>
+        <RowSpaceWrapper gap={10}>
+          <Thumbnail imageUrl={image[0]} />
+          <TextContainer>
+            <Name>{name}</Name>
+            <DividerWithMargin />
+            <Price>{price}원</Price>
+            <Description>{description}</Description>
+          </TextContainer>
+        </RowSpaceWrapper>
+      </Container>
+      <Divider />
+    </ColumnSpaceWrapper>
+  );
+};
+
+export default AccommodationsCard;
--- src/routes/accommodations/AccommodationsPresenter.tsx
+++ src/routes/accommodations/AccommodationsPresenter.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import PageLayoutWithDivide from '../../components/Layouts/PageLayoutWithDivide';
+import AccommodationsCard from '../../components/Cards/AccmmodationsCard';
+import ColumnSpaceWrapper from '../../components/Wrappers/ColumnSpaceWrapper';
 
-const AccommodationsPresenter: React.FC = () => {
+interface AccommodationsDataType {
+  address: string;
+  name: string;
+  price: string;
+  image: string[];
+  user: string;
+}
+
+interface AccommodationsPresenterProps {
+  accommodationsData: AccommodationsDataType[];
+}
+
+const MapAndListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const Half = styled.div`
+  display: flex;
+  width: 50%;
+`;
+const MapSection = styled(Half)`
+  width: 50%;
+`;
+const ListSection = styled(Half)`
+  width: 50%;
+`;
+
+const AccommodationsPresenter: React.FC<AccommodationsPresenterProps> = ({
+  accommodationsData,
+}) => {
   return (
     <PageLayoutWithDivide
       image="accommodationsPageImage.jpeg"
@@ -9,7 +41,21 @@ const AccommodationsPresenter: React.FC = () => {
       title="숙소"
       description="모두가 함께 즐기는 세계 각지의 멋진 숙소"
     >
-      <div>HELLO TEST</div>
+      <MapAndListContainer>
+        <ListSection>
+          <ColumnSpaceWrapper gap={0}>
+            {accommodationsData.map((accommodations) => (
+              <AccommodationsCard
+                name={accommodations.name}
+                image={accommodations.image}
+                price={accommodations.price}
+                description="완전 안전, 완전 깨끗, 완전 쾌적. 여러분들의 행복한 여행을 위한 모든 것."
+              />
+            ))}
+          </ColumnSpaceWrapper>
+        </ListSection>
+        <MapSection>Map</MapSection>
+      </MapAndListContainer>
     </PageLayoutWithDivide>
   );
 };
