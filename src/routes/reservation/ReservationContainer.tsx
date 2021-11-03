6d2568bd29b1f6d8a3382d1d3d81dfd2a1edce25
call5h5ong <call5h5ong@gmail.com>
2021-11-03 12:32:31 +0900

    feat : 예약 페이지 상단 부분 구현
    건물 이름, 건물 주소, 세부 이미지 구현함

--- src/routes/reservation/ReservationContainer.tsx
+++ src/routes/reservation/ReservationContainer.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useAxios from '../../hooks/useAxios';
 import ReservationPresenter from './ReservationPresenter';
 
 const ReservationContainer: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const { data, error, loading } = useAxios<AccommodationsDataType>({
+    url: `http://localhost:4000/accommodations/${id}`,
+  });
 
-  if (id) {
-    return <ReservationPresenter id={id} />;
+  if (!loading && data) {
+    return <ReservationPresenter accommodationsData={data} />;
   } else {
     return <div>loading...</div>;
   }
--- src/routes/reservation/ReservationPresenter.tsx
+++ src/routes/reservation/ReservationPresenter.tsx
@@ -2,20 +2,73 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface ReservationPresenterProps {
-  /** Accommodations ID */
-  id: string;
+  /** Accommodations Data */
+  accommodationsData: AccommodationsDataType;
 }
 
-const ImageSection = styled.div``;
+const LootContainer = styled.div`
+  padding-left: 150px;
+  padding-right: 150px;
+`;
+const TopTitleSection = styled.div`
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: bold;
+  padding-bottom: 10px;
+`;
+const SubTitle = styled.div``;
+const ImageSection = styled.div`
+  display: flex;
+  // child들이 border-radius를 적용받을 수 있게 만들기 위해서
+  overflow: hidden;
+  flex-direction: row;
+  /* background-color: black; */
+  width: 100%;
+  height: 450px;
+  border-radius: 10px;
+`;
+const Grid = styled.div`
+  display: grid;
+  flex: 1;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 7px;
+`;
+const BaseImage = styled.div<{ imageUrl: string }>`
+  background-image: ${(props): string => `url(${props.imageUrl})`};
+  background-size: cover;
+  background-position: center;
+`;
+const SmallImage = styled(BaseImage)``;
+const LargeImage = styled(BaseImage)`
+  flex: 1;
+  margin-right: 7px;
+`;
 const ReservationSection = styled.div``;
 
-const ReservationPresenter: React.FC<ReservationPresenterProps> = ({ id }) => {
+const ReservationPresenter: React.FC<ReservationPresenterProps> = ({
+  accommodationsData,
+}) => {
   return (
-    <>
-      <div>{id}</div>
-      <ImageSection />
+    <LootContainer>
+      <TopTitleSection>
+        <Title>{accommodationsData.name}</Title>
+        <SubTitle>{accommodationsData.address}</SubTitle>
+      </TopTitleSection>
+      <ImageSection>
+        <LargeImage imageUrl={accommodationsData.image[0]} />
+        <Grid className="image-grid">
+          <SmallImage imageUrl={accommodationsData.image[1]} />
+          <SmallImage imageUrl={accommodationsData.image[2]} />
+          <SmallImage imageUrl={accommodationsData.image[3]} />
+          <SmallImage imageUrl={accommodationsData.image[4]} />
+        </Grid>
+      </ImageSection>
       <ReservationSection />
-    </>
+    </LootContainer>
   );
 };
 
