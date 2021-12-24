ffee7dbcf2375093e621c3b6982aca65693de4c9
call5h5ong <call5h5ong@gmail.com>
2021-12-24 16:50:15 +0900

    type: 타입들의 위치 변경
    `components.d.ts`는 본래 컴포넌트의 타입이 선언되야 했음. 근데 다른 데이터들의 타입까지 같이 선언 되면서 일관성이 없어짐. 그래서 새로  데이터 관련 타입을 `data.d.ts`를 만들어 옮겨줌.

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -9,31 +9,3 @@ type DashboardProps = Pick<
   UserDataType,
   'email' | 'accommodations' | 'reservations'
 >;
-
-interface ReservationType {
-  start: string;
-  end: string;
-}
-interface ReservationDataType {
-  id: string;
-  userId: string;
-  /** 예약한 숙소의 아이디 */
-  accommodationsId: string;
-  accommodationData: AccommodationsDataType;
-  /** 예약한 날짜 */
-  reservationDate: ReservationType;
-  /** 발행 날짜 */
-  issuedDate: string;
-}
-interface AccommodationsDataType {
-  id: string;
-  address: string;
-  name: string;
-  price: number;
-  image: string[];
-  user: string;
-  lng: number;
-  lat: number;
-  // 로그인 한 유저의 해당 숙소 예약 정보
-  requestUserReservation: ReservationDataType;
-}
