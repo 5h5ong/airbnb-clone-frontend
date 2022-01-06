86ea996b6e0dcc4cfb5180d3525eb044ec9de997
call5h5ong <call5h5ong@gmail.com>
2022-01-06 21:04:15 +0900

    fix: 월이 -1 되어 표시됨

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,5 +1,7 @@
 /**
  * String 형식으로 들어온 날짜를 년/월/일 형식으로 바꿈
+ * @remarks
+ * 자바스크립트의 Date class는 month가 0부터 시작함. 진짜 month를 얻기 위해서는 1을 더해줘야 함.
  */
 export const isoStringToRealDateString = (isoString: string) => {
   const realDate = new Date(isoString);
@@ -10,5 +12,5 @@ export const isoStringToRealDateString = (isoString: string) => {
     realDate.getDate(),
   ];
 
-  return `${year}년 ${month}월 ${date}일`;
+  return `${year}년 ${month + 1}월 ${date}일`;
 };
